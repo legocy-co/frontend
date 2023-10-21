12cc7b00cdeaa6f5ab16888b7cd5fbfdf86db207
Tikhon Kozlov <STPKBFF@yandex.ru>
2023-10-21 19:21:49 +0200

    wip chat

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import './App.css';
+import { WeavyClient, WeavyProvider, Chat } from '@weavy/uikit-react';
+
+const weavyClient = new WeavyClient({
+    url: "https://6c4406765dbc4323974bdd9c9ee2f636.weavy.io",
+    tokenFactory: async () => "wyu_gOis5vBqiDAix29sOtwINugDSWOYOe3THOPu"
+});
 
 function App() {
-  return (
-    <div className="App">
-      <p>Hello, Legocy World!</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <WeavyProvider client={weavyClient}>
+                <Chat uid="demochat" />
+            </WeavyProvider>
+        </div>
+    )
 }
 
 export default App;
