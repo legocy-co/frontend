3acfdf6df18aa05cbc1d9287a195f8b73fa8407a
Tikhon Kozlov <STPKBFF@yandex.ru>
2023-10-21 20:34:16 +0200

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { WeavyClient, WeavyProvider, Chat } from '@weavy/uikit-react';
+import "@weavy/uikit-react/dist/css/weavy.css";
 
 const weavyClient = new WeavyClient({
     url: "https://6c4406765dbc4323974bdd9c9ee2f636.weavy.io",
-    tokenFactory: async () => "wyu_gOis5vBqiDAix29sOtwINugDSWOYOe3THOPu"
+    tokenFactory: async () => "wyu_R1ov77mlLc3m27hJpXCYa3xtjJ37p20lPGfZ"
 });
 
 function App() {
