a046495d0e95824ab552435147bf95a8a99bc2b8
milvalen <artemyerkov@gmail.com>
2023-12-01 23:26:53 +0300

    vite react ts init

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import React from 'react';
-import { WeavyClient, WeavyProvider, Chat } from '@weavy/uikit-react';
-import "@weavy/uikit-react/dist/css/weavy.css";
-
-const weavyClient = new WeavyClient({
-    url: "https://6c4406765dbc4323974bdd9c9ee2f636.weavy.io",
-    tokenFactory: async () => "wyu_R1ov77mlLc3m27hJpXCYa3xtjJ37p20lPGfZ"
-});
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-    return (
-        <div className="App">
-            <WeavyProvider client={weavyClient}>
-                <Chat uid="demochat" />
-            </WeavyProvider>
-        </div>
-    )
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
