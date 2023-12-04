2ff0e714359be489d440ccd1afa70bc0a1e6d739
milvalen <artemyerkov@gmail.com>
2023-12-04 23:48:26 +0300

    wip (Footer)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,14 @@ const Footer = () => {
   return (
       <footer>
         <div className="footer--group">
-
+          <div className="footer--contacts">
+            <p>legocy.info@gmail.com</p>
+            <p>+7 898 384 9884</p>
+            <img src="/src/assets/icons/social.svg" alt=""/>
+          </div>
+          <div className="footer--policy">
+            <img src="/logo-dark.svg" alt=""/>
+          </div>
         </div>
       </footer>
   );
