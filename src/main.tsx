32168d40ea456c064bf65bb16cb5362af63af61e
milvalen <artemyerkov@gmail.com>
2023-12-14 23:54:53 +0300

    update .gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import './app/index.pcss';
 import './assets/styles/reset.scss';
 import './assets/styles/global.scss';
-
 import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client'
-import AppRouter from './routes'
+import ReactDOM from 'react-dom/client';
+import AppRouter from './routes';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
@@ -14,4 +13,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <AppRouter />
     </DndProvider>
   </BrowserRouter>,
-)
+);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import AuthPage from '../pages/AuthPage';
 import SignUpPage from '../pages/SignUpPage';
-import SignInPage from "../pages/SignInPage";
+import SignInPage from '../pages/SignInPage';
 import RootPage from '../pages/RootPage';
 
 const AppRouter = () => {
   return (
-      <Routes>
-        <Route path="/auth/" element={<AuthPage />} />
-        <Route path="/auth/sign-up" element={<SignUpPage />} />
-        <Route path="/auth/sign-in" element={<SignInPage />} />
-        <Route path="/" element={<RootPage />} />
-      </Routes>
-  )
-}
+    <Routes>
+      <Route path="/auth/" element={<AuthPage />} />
+      <Route path="/auth/sign-up" element={<SignUpPage />} />
+      <Route path="/auth/sign-in" element={<SignInPage />} />
+      <Route path="/" element={<RootPage />} />
+    </Routes>
+  );
+};
 
 export default AppRouter;
