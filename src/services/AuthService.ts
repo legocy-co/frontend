dd0fa090a2aa0321a77973fb07474f6b4847c01f
milvalen <artemyerkov@gmail.com>
2023-12-26 14:30:53 +0300

    test fix 405 build

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -86,7 +86,7 @@ const GetAccessTokenHeader = () => {
   return `Bearer ${config.accessToken}`;
 };
 
-axios.defaults.baseURL = '/api/v1';
+axios.defaults.baseURL = 'http://localhost:5173/api/v1';
 axios.defaults.headers.common.Authorization = IsAuthorized()
   ? GetAccessTokenHeader()
   : '';
