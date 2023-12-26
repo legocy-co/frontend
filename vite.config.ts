15f1959e7e06572e39d7d2fdf93704923a21a0ba
milvalen <artemyerkov@gmail.com>
2023-12-26 16:16:04 +0300

    test build secure proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 // export default defineConfig({
@@ -22,14 +23,13 @@ export default ({ mode }) => {
 
   return defineConfig({
     plugins: [react()],
-
     server: {
       host: true,
       proxy: {
         '/api': {
           target: process.env.VITE_API_ENDPOINT,
           changeOrigin: true,
-          secure: false,
+          secure: true,
         },
       },
     },
