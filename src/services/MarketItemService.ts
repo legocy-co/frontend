4c14cc9cb04819f1b5e96df527dc4c65a4217790
milvalen <artemyerkov@gmail.com>
2023-12-27 22:44:26 +0300

    handle incorrect parse

--- src/services/MarketItemService.ts
+++ src/services/MarketItemService.ts
@@ -1,4 +1,15 @@
-import { MarketItem } from '../types/MarketItemType.ts';
+import { MarketItem, MarketItemSchema } from '../types/MarketItemType.ts';
+import axios from 'axios';
+import { handleIncorrectParse } from './ErrorHandlers.ts';
+
+const GetMarketItems = async (): Promise<MarketItem[]> => {
+  const response = await axios.get<MarketItem[]>('/market-items/');
+  const result = MarketItemSchema.array().safeParse(response.data);
+  if (!result.success)
+    return handleIncorrectParse(result.error, 'GetMarketItems');
+
+  return result.data;
+};
 
 interface MarketItemService {
   GetMarketItems: () => Promise<MarketItem[]>;
@@ -8,3 +19,7 @@ interface MarketItemService {
   UpdateMarketItem: (id: number, marketItem: MarketItem) => Promise<MarketItem>;
   DeleteMarketItem: (id: number) => Promise<boolean>;
 }
+
+// export const MarketItemService: MarketItemService = {
+//   GetMarketItems: GetMarketItems,
+// };
--- src/types/MarketItemType.ts
+++ src/types/MarketItemType.ts
@@ -5,14 +5,23 @@ import { MarketItemImageSchema } from './MarketItemImage.ts';
 
 export type MarketItem = z.infer<typeof MarketItemSchema>;
 
+const SetStates = [
+  'BRAND_NEW',
+  'BOX_OPENED',
+  'BAGS_OPENED',
+  'BUILT_WITH_BOX',
+  'BUILT_WITHOUT_BOX',
+  'BUILT_PIECES_LOST',
+] as const;
+
 export const MarketItemSchema = z.object({
-  id: z.number(),
+  id: z.number().optional(),
   price: z.number(),
   location: z.string(),
   lego_set: LegoSetSchema,
   seller: UserSchema,
-  status: z.string(),
-  set_state: z.string(),
-  description: z.string().min(1),
+  status: z.string().optional(),
+  set_state: z.enum(SetStates),
+  description: z.string(),
   images: z.array(MarketItemImageSchema).optional(),
 });
