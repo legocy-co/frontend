a10fe12408a510a543de08a1dfb6f42bc5bbbd56
milvalen <artemyerkov@gmail.com>
2024-01-12 12:50:17 +0300

    userprofilepage get method

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,5 +1,30 @@
-// import { UserProfile } from '../types/UserProfileType';
-//
-// interface UserService {
-//   GetUserProfilePage: (id: string) => Promise<UserProfile>;
-// };
+import { UserProfile, UserProfileSchema } from '../types/UserProfileType';
+import axios from 'axios';
+import { handleIncorrectParse } from './ErrorHandlers.ts';
+
+interface UserService {
+  GetUserProfilePage: (id: string) => Promise<UserProfile>;
+}
+
+type UserProfileResponse = {
+  market_items: object | object[];
+  user: object;
+  user_reviews: object | object[];
+};
+
+const GetUserProfilePage = async (id: string): Promise<UserProfile> => {
+  const response = await axios.get<UserProfileResponse>('/users/profile/' + id);
+  const result = UserProfileSchema.safeParse(response);
+  if (!result.success)
+    return handleIncorrectParse(
+      result.error,
+      'GetUserProfilePage',
+      "Can't get user profile page"
+    );
+
+  return result.data;
+};
+
+export const userService: UserService = {
+  GetUserProfilePage: GetUserProfilePage,
+};
