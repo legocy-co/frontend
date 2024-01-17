6a99f37062468dfcc31b4e94a1060f4e59091cd1
milvalen <artemyerkov@gmail.com>
2024-01-17 22:33:44 +0300

    form field adapters

--- src/features/market-item/model.ts
+++ src/features/market-item/model.ts
@@ -1,8 +1,6 @@
 import { createForm } from 'effector-forms';
 import { createRule } from '../../services/utils.ts';
 import { z } from 'zod';
-import objectKeysToZodEnum from '../../shared/lib/zod.ts';
-import { setStates } from '../../types/MarketItemType.ts';
 import { createGate } from 'effector-react';
 
 export const Gate = createGate<{ id: string | null }>();
@@ -19,36 +17,24 @@ export const form = createForm({
             .int()
             .nonnegative()
             .nullable()
-            .refine((value) => value !== null, 'required field'),
+            .refine((value) => value !== null, 'Missing lego set ID'),
         }),
       ],
     },
-    set_state: {
+    condition: {
       init: '',
       rules: [
         createRule({
           name: 'set_states',
-          schema: objectKeysToZodEnum(setStates),
+          schema: z.string().min(1, 'Missing condition'),
         }),
       ],
     },
     description: {
       init: '',
-      rules: [
-        createRule({
-          name: 'description',
-          schema: z.string().min(1, 'required field'),
-        }),
-      ],
     },
     location: {
       init: '',
-      rules: [
-        createRule({
-          name: 'location',
-          schema: z.string().min(1, 'required field'),
-        }),
-      ],
     },
     price: {
       init: null as null | number,
@@ -59,7 +45,7 @@ export const form = createForm({
             .number()
             .nonnegative()
             .nullable()
-            .refine((value) => value !== null, 'required field'),
+            .refine((value) => value !== null, 'Missing price'),
         }),
       ],
     },
--- src/shared/ui/form-adapters.tsx
+++ src/shared/ui/form-adapters.tsx
@@ -1,9 +1,17 @@
 import { Field, useField } from 'effector-forms';
 import { Input, InputProps } from './input';
+import React from 'react';
+import { Textarea } from './textarea.tsx';
+import clsx from 'clsx';
 
 type FormAdapterProps<T> = {
   field: Field<T>;
 } & Omit<InputProps, 'isInvalid'>;
+
+export type SelectFieldOption = {
+  value: string;
+  label: string;
+};
 
 export const TextFieldAdapter = ({
   field,
@@ -20,3 +28,75 @@ export const TextFieldAdapter = ({
     />
   );
 };
+
+export const NumberFieldAdapter = ({
+  field,
+  ...props
+}: FormAdapterProps<number | null>) => {
+  const { value, onChange, hasError } = useField(field);
+
+  return (
+    <Input
+      type="number"
+      value={value ?? ''}
+      onChange={handleNumberFieldChange(onChange)}
+      isInvalid={hasError()}
+      {...props}
+    />
+  );
+};
+
+export const SelectFieldAdapter = ({
+  field,
+  defaultOptionValue,
+  options,
+}: {
+  options: SelectFieldOption[];
+  defaultOptionValue: string;
+  field: Field<string>;
+}) => {
+  const { value, onChange, hasError } = useField(field);
+  const isInvalid = hasError();
+
+  return (
+    <select
+      value={value ?? defaultOptionValue}
+      onChange={(ev) => onChange(ev.currentTarget.value)}
+      className={clsx(
+        'block w-[343px] h-[44px] border border-solid border-slate rounded-xl text-charcoal indent-3 pr-10 outline-0 mb-3.5',
+        { 'bg-rose': isInvalid }
+      )}
+    >
+      {options.map(({ value, label }) => (
+        <option key={value} value={value} className="">
+          {label}
+        </option>
+      ))}
+    </select>
+  );
+};
+
+export const TextareaFieldAdapter = ({
+  field,
+  className,
+  labelText,
+}: FormAdapterProps<string>) => {
+  const { value, onChange, hasError } = useField(field);
+
+  return (
+    <Textarea
+      labelText={labelText}
+      className={className}
+      isInvalid={hasError()}
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    />
+  );
+};
+
+function handleNumberFieldChange(onChange: (value: number | null) => void) {
+  return (ev: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = ev.target;
+    value === '' || isNaN(+value) ? onChange(null) : onChange(+value);
+  };
+}
