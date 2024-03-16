3b0cfffed37d965ac43bc2f9c532e96810b38f31
milvalen <artemyerkov@gmail.com>
2024-03-16 21:58:09 +0300

    init

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,11 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
   readonly VITE_API_ENDPOINT: string;
+  readonly QB_APPLICATION_ID: number;
+  readonly QB_AUTH_KEY: string;
+  readonly QB_AUTH_SECRET: string;
+  readonly QB_ACCOUNT_KEY: string;
+  readonly QB_LOGIN_PASSWORD: string;
 }
 
 interface ImportMeta {
