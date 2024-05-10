37abcf40af3e42afc056f67fd8ed7bf029eb9aef
milvalen <artemyerkov@gmail.com>
2024-05-10 19:36:49 +0300

    handle uploads events

--- src/pages/user-profile-pages/uploads/model.ts
+++ src/pages/user-profile-pages/uploads/model.ts
@@ -1,6 +1,17 @@
 import { createGate } from 'effector-react';
-import { createEvent, createStore } from 'effector';
+import {
+  attach,
+  createEffect,
+  createEvent,
+  createStore,
+  sample,
+} from 'effector';
 import { MarketItem } from '../../../types/MarketItemType.ts';
+import { userService } from '../../../services/UserService.ts';
+import {
+  $marketItemCells,
+  toMarketItemCells,
+} from '../../../components/MarketItemsList/model.ts';
 
 export const gate = createGate();
 
@@ -8,4 +19,39 @@ export const $status = createStore<string>('ACTIVE');
 
 export const $marketItems = createStore<MarketItem[]>([]);
 
-export const setStatus = createEvent<string>();
+export const statusChanged = createEvent<string>();
+
+const GetUserFX = createEffect(() => userService.GetUserProfilePage());
+
+const fetchMarketItemsByStatusFX = attach({
+  source: { marketItems: $marketItems, status: $status },
+  effect: ({ marketItems, status }) =>
+    marketItems.filter((item) => item.status === status),
+});
+
+sample({
+  clock: gate.open,
+  target: GetUserFX,
+});
+
+sample({
+  source: GetUserFX.doneData,
+  fn: (user) => user.marketItems,
+  target: $marketItems,
+});
+
+sample({
+  clock: [$marketItems, $status],
+  target: fetchMarketItemsByStatusFX,
+});
+
+sample({
+  source: fetchMarketItemsByStatusFX.doneData,
+  fn: toMarketItemCells,
+  target: $marketItemCells,
+});
+
+sample({
+  source: statusChanged,
+  target: $status,
+});
