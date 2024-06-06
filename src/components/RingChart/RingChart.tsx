026844eb7f9d7d1477458ee0f85208f75b6e6b1d
milvalen <artemyerkov@gmail.com>
2024-06-07 02:15:07 +0300

    find gradient color

--- src/components/RingChart/RingChart.tsx
+++ src/components/RingChart/RingChart.tsx
@@ -4,13 +4,27 @@ import { Cell, Pie, PieChart } from 'recharts';
 
 const RingChart = () => {
   const data = [
-    { name: 'Group A', value: 30 },
-    { name: 'Group B', value: 20 },
-    { name: 'Group C', value: 25 },
-    { name: 'Group D', value: 10 },
+    { name: 'Group A', value: 10 },
+    { name: 'Group B', value: 9 },
+    { name: 'Group C', value: 9 },
+    { name: 'Group D', value: 8 },
+    { name: 'Group E', value: 7 },
+    { name: 'Group E', value: 6 },
     { name: 'Group E', value: 5 },
   ];
-  const COLORS = ['#FFD540', '#FFB340', '#DF5F36', '#832222', '#691313'];
+
+  const colors = Array.from({ length: data.length }, (_, i) => {
+    const findColor = (left: number, right: number): number =>
+      // left + (right - left) * (i / (data.length - 1));
+      left + (i * (right - left)) / (data.length - 1);
+
+    return `rgb(${findColor(215, 255)}, ${findColor(77, 228)}, ${findColor(
+      33,
+      133
+    )})`;
+  });
+
+  console.log(colors);
 
   //TODO: layout pie chart
   return (
@@ -23,17 +37,18 @@ const RingChart = () => {
         <Pie
           data={data}
           innerRadius={60}
-          rotate={-150}
+          startAngle={180}
+          endAngle={-180}
+          cornerRadius={9999}
+          paddingAngle={-15}
           fill="#8884d8"
           dataKey="value"
         >
           {data.map((_, index) => (
             <Cell
               key={`cell-${index}`}
-              stroke={COLORS[index % COLORS.length]}
-              fill={COLORS[index % COLORS.length]}
-              z={index}
-              opacity={0.9}
+              stroke={colors[index]}
+              fill={colors[index]}
             />
           ))}
         </Pie>
