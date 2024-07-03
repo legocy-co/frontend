5cb7e8d5252411d959c6fbe11d9239a3d1119f20
milvalen <artemyerkov@gmail.com>
2024-07-04 01:35:10 +0300

    layout our mission

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,10 +1,16 @@
 import { Button } from '../../shared/ui/button';
 import LegoHeadIcon from '../../assets/icons/lego-head.svg?react';
+import LocationIcon from '../../assets/icons/location.svg?react';
 import * as lib from './lib';
+import { useNavigate } from 'react-router-dom';
+import { authService } from '../../services/AuthService.ts';
+import clsx from 'clsx';
 
-// TODO: layout our mission
+// TODO: layout our goals
 
 export const AboutUsPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col items-center justify-start gap-28 w-full">
       <div className="flex items-center bg-about-us bg-center bg-no-repeat justify-center w-screen h-min-[570px] p-20 bg-cover mt">
@@ -54,11 +60,44 @@ export const AboutUsPage = () => {
           everything we do. Meet the faces behind LEGOCY and discover the
           driving force behind our mission to unite LEGO enthusiasts globally.
         </p>
-        <div className="flex items-center justify-center gap-6 flex-wrap">
+        <div className="flex items-center justify-center gap-14 flex-wrap">
           {lib.team.map((teammate) => (
-            <Teammate name={teammate.name} picture={teammate.picture} />
+            <Teammate
+              key={teammate.name}
+              name={teammate.name}
+              picture={teammate.picture}
+            />
           ))}
         </div>
+      </div>
+      <div className="flex flex-col gap-8 items-center justify-center">
+        <h1 className="text-bh font-bold">Our mission</h1>
+        <div className="mt-14 min-w-96 w-[95%] rounded-xl bg-pagesize flex lg:flex-col gap-3 lg:gap-12 items-center justify-center dark:bg-dark px-10 pl-20 py-10 lg:pt-20 lg:pl-10 text-center">
+          <div className="h-[948px] w-max-[948px] flex flex-col lg:flex-row justify-between lg:w-[77%] lg:h-0 border border-solid border-state dark:border-white">
+            {lib.spotPics.map((spotPic) => (
+              <div className="relative" key={spotPic}>
+                <Spot picture={spotPic} />
+              </div>
+            ))}
+          </div>
+          <p className="w-28 text-left lg:text-center lg:w-[875px] xl:w-[1094px] text-lg">
+            At LEGOCY, our mission is to connect LEGO enthusiasts worldwide,
+            creating a vibrant, inclusive community where passion for LEGO
+            transcends borders. We strive to offer an innovative platform that
+            enriches the LEGO experience, making it more accessible, enjoyable,
+            and rewarding for enthusiasts of all ages. Together, we build more
+            than just models; we build friendships, knowledge, and a global
+            community united by a shared love for LEGO.
+          </p>
+        </div>
+        <Button
+          onClick={() => navigate('/auth/sign-up')}
+          className={clsx('!h-12 !w-[278px] !rounded-xl', {
+            hidden: authService.IsAuthorized(),
+          })}
+        >
+          Create account
+        </Button>
       </div>
     </div>
   );
@@ -66,18 +105,31 @@ export const AboutUsPage = () => {
 
 const Teammate = ({ name, picture }: lib.Teammate) => {
   return (
-    <div className="flex flex-col gap-2 items-center justify-center">
+    <div className="flex flex-col gap-4 items-center justify-center">
       <div className="w-80 relative sm:w-[387px] aspect-[1.78] rounded-xl bg-pagesize dark:bg-dark flex justify-center items-center">
         <div className="w-[126px] sm:w-[153px] aspect-square rounded-full bg-legocy" />
         <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2">
           <img
             src={picture}
             alt=""
-            className="w-36 sm:w-24 aspect-[0.53] object-cover overflow-visible"
+            className="w-20 sm:w-24 aspect-[0.53] object-cover overflow-visible"
           />
         </div>
       </div>
       <p className="text-[1.375rem]">{name}</p>
+    </div>
+  );
+};
+
+const Spot = ({ picture }: { picture: string }) => {
+  return (
+    <div className="absolute flex lg:flex-col gap-8 bottom-[-39px] sm:bottom-[-45px] right-[-5px] justify-end items-center lg:gap-8 lg:bottom-[-8px]">
+      <img
+        src={picture}
+        alt=""
+        className="h-20 object-cover object-center overflow-visible"
+      />
+      <LocationIcon className="iconfills w-4 h-6 -rotate-90 lg:rotate-0 overflow-visible" />
     </div>
   );
 };
--- src/pages/AboutUsPage/lib.ts
+++ src/pages/AboutUsPage/lib.ts
@@ -1,6 +1,11 @@
 import LyubaVozzhova from '../../assets/pics/lyuba-vozzhova.png';
 import TikhonKozlov from '../../assets/pics/tikhon-kozlov.png';
 import ArtemyErkov from '../../assets/pics/artemy-erkov.png';
+import LegoPagoda from '../../assets/pics/lego-pagoda.png';
+import LegoLiberty from '../../assets/pics/lego-liberty.png';
+import LegoGlobe from '../../assets/pics/lego-globe.png';
+import LegoNotreDame from '../../assets/pics/lego-notre-dame.png';
+import LegoPyramid from '../../assets/pics/lego-pyramid.png';
 
 export type Teammate = { name: string; picture: string };
 
@@ -8,4 +13,12 @@ export const team: Teammate[] = [
   { name: 'Lyuba Vozzhova', picture: LyubaVozzhova },
   { name: 'Tikhon Kozlov', picture: TikhonKozlov },
   { name: 'Artemy Erkov', picture: ArtemyErkov },
+];
+
+export const spotPics: string[] = [
+  LegoPagoda,
+  LegoLiberty,
+  LegoGlobe,
+  LegoNotreDame,
+  LegoPyramid,
 ];
