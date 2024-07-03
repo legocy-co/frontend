857b4a1c5b6e493256ee05ae79bcf77217ef34ec
milvalen <artemyerkov@gmail.com>
2024-07-03 21:04:32 +0300

    about us element

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,11 +1,13 @@
 import { Button } from '../../shared/ui/button.tsx';
+import LegoHeadIcon from '../../assets/icons/lego-head.svg?react';
 
+// TODO: meet the team
 export const AboutUsPage = () => {
   return (
-    <div className="flex flex-col items-center justify-start w-screen h-full">
+    <div className="flex flex-col items-center justify-start gap-28 w-screen h-full">
       <div className="flex items-center bg-about-us bg-center bg-no-repeat justify-center w-screen h-min-[570px] p-20 bg-cover mt">
         <div className="min-w-96 w-[82%] p-20 bg-white dark:bg-dark rounded-xl flex flex-col justify-between items-center gap-12">
-          <h1 className="!text-wrap text-center w-full !m-0 text-bh font-bold">
+          <h1 className="!text-wrap text-center w-full text-bh font-bold">
             The Ultimate Web Hub for LEGO Enthusiasts
           </h1>
           <p className="text-lg text-center max-w-[922px]">
@@ -24,6 +26,18 @@ export const AboutUsPage = () => {
             </Button>
           </div>
         </div>
+      </div>
+      <div className="relative min-w-96 w-[95%] rounded-xl bg-pagesize dark:bg-dark px-10 pb-10 pt-20 text-center text-[1.625rem]">
+        <div className="absolute flex justify-center items-center top-[-28px] left-1/2 transform -translate-x-1/2">
+          <LegoHeadIcon />
+          <h1 className="absolute text-nowrap text-bh font-bold">About Us</h1>
+        </div>
+        “At LEGOCY, we are not just another company; we are a passionate
+        community of LEGO collectors. Our love for LEGO has driven us to create
+        a unique web-service dedicated to making your LEGO collecting experience
+        richer and more exciting. We understand the joy of building, the thrill
+        of discovering new sets, and the excitement of sharing your collection
+        with like-minded enthusiasts.“
       </div>
     </div>
   );
